d06e4d678487da063e1573e688483a520a622eff
Travis71 <elliottalvarez2012@gmail.com>
2021-01-29 23:46:27 -0600

    Dev Branch: Changes - extension.ts: More organized (maybe). - README.md: Badges, more images, and changes in ## Formatter Options.

--- README.md
+++ README.md
@@ -2,15 +2,28 @@
     <img src="https://raw.githubusercontent.com/Travis71i/clean-css-vscode/master/images/clean-css-vscode-v1.png" alt="Clean CSS for VSCode" width="800px"/>
 </h1>
 
+![Visual Studio Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/travis71i.clean-css-vscode)
+![Visual Studio Marketplace Installs](https://img.shields.io/visual-studio-marketplace/i/travis71i.clean-css-vscode)
+![David](https://david-dm.org/Travis71i/clean-css-vscode.svg)
+![LICENSE](https://img.shields.io/github/license/Travis71i/clean-css-vscode)
+
 CSS file optimizer. Using [Clean CSS](https://github.com/jakubpawlowicz/clean-css) and adapting it to work as an extension of VSCode. 
 
 **Note:** The functionality of this extension depends entirely on Clean CSS. Thanks to [jakubpawlowicz](https://github.com/jakubpawlowicz) for creating and managing it!
 
 # Features
-
 - Compress CSS files using Clean CSS optimizer.
+
+![Before //dev](https://raw.githubusercontent.com/Travis71i/clean-css-vscode/dev/images/css_1.png)
+![After //dev](https://raw.githubusercontent.com/Travis71i/clean-css-vscode/deb/images/css_2.png)
+
 - Quickly format using different presets, in the editor context menu.
+
+![Editor Context Menu //dev](https://raw.githubusercontent.com/Travis71i/clean-css-vscode/dev/images/editor_context_menu.png)
+
 - Use "Clean CSS: Format Document" command (Shift+Alt+F) to format using user settings.
+
+![Settings UI //dev](https://raw.githubusercontent.com/Travis71i/clean-css-vscode/dev/images/settings_ui.png)
 
 # Extension Settings
 
@@ -24,33 +37,9 @@ CSS file optimizer. Using [Clean CSS](https://github.com/jakubpawlowicz/clean-cs
 
 ## Formatter Options
 
-All options in this group go directly to the formatter.
-See [Clean CSS: Options](https://github.com/jakubpawlowicz/clean-css#constructor-options) for more info about it.
-
-### List of Supported Settings in Settings UI
-
-#### Compatibility
-
-- `cleanCSS.formatterOptions.compatibility` Type: string
-
-#### Format
-
-- `cleanCSS.formatterOptions.format.breaks` Type: object
-- `cleanCSS.formatterOptions.format.breakWith` Type: string
-- `cleanCSS.formatterOptions.format.indentBy` Type: number
-- `cleanCSS.formatterOptions.format.indentWith` Type: string
-- `cleanCSS.formatterOptions.format.spaces` Type: object
-- `cleanCSS.formatterOptions.format.wrapAt` Type: number or boolean
-- `cleanCSS.formatterOptions.format.semicolonAfterLastProperty` Type: boolean
-
-#### Inline
-
-- `cleanCSS.formatterOptions.inline` Type: Array
-
-#### Levels
-
-- `cleanCSS.formatterOptions.level.1` Type: object
-- `cleanCSS.formatterOptions.level.2` Type: object
+All options in this group go directly to the formatter. `cleanCSS.formatterOptions` object is an `options` object of Clean CSS.
+    
+See [Clean CSS: Options](https://github.com/jakubpawlowicz/clean-css#constructor-options) for more info about the `options` object.
 
 # Release Notes
 See [CHANGELOG.md](CHANGELOG.md) for more info.
@@ -63,5 +52,5 @@ See [CHANGELOG.md](CHANGELOG.md) for more info.
  - **"Clean CSS: Fast Beautify" Format** command to format the document in a nice way, using default settings.
  - **"Clean CSS: Fast Keep Breaks" Format** command to format the document in a compact but more readable way, using default settings.
  - **"Format Document (Clean CSS)" submenu** in the editor context menu with all 4 commands listed above.
- - **Key binding: "Format Document" (Ctrl+Shift+F)** to use `Clean CSS: Format Document` command.
+ - **Key binding: "Format Document" (Shift+Alt+F)** to use `Clean CSS: Format Document` command.
  - **Clean CSS output channel** to see more information about formatting. Such as some error and warning messages.
--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,78 @@
 import * as vscode from 'vscode';
 const cleanCSS = require('clean-css');
 var debugCh: vscode.OutputChannel;
+var options: vscode.WorkspaceConfiguration;
 
+// Activating extension
 export function activate(context: vscode.ExtensionContext) {
-	let disp = [
-		vscode.commands.registerCommand('clean-css-vscode.Format', () => format()),
-		vscode.commands.registerCommand('clean-css-vscode.FastBeauty', () => format({format: 'beautify'})),
-		vscode.commands.registerCommand('clean-css-vscode.FastKBreaks', () => format({format: 'keep-breaks'})),
-		vscode.commands.registerCommand('clean-css-vscode.FastCompact', () => format({}))
-	];
-  disp.forEach(command => {context.subscriptions.push(command);});
+  // Add commands to the subscription array.
+  context.subscriptions.splice(0,0,...CommandList());
+  // Creating the Output Channel for warning, errors and info.
   debugCh = vscode.window.createOutputChannel("Clean CSS");
 }
 
-function format(option?: object){
-  let editor = vscode.window.activeTextEditor;
-  
-	if (editor !== undefined && editor.document.languageId == 'css'){
-    let doc = editor.document;
-    let options = vscode.workspace.getConfiguration('cleanCSS');
-    new cleanCSS(option??options.get('formatterOptions')).minify(editor.document.getText(),
-    function(error: object,output: {styles: string}) {
-      if(!error){    
-        editor?.edit(edit => {
-          edit.replace(new vscode.Range(0,0,doc.lineCount-1,doc.lineAt(doc.lineCount-1).text.length),
-          output.styles)
-        });
-        if(options.get<boolean>('debugTool.showMessage')) debugInfo(output);
-      }else {
-        vscode.window.showErrorMessage(error.toString())
-      }
-    });
-	} else { return; }
+// List of commands to push. If you want to
+function CommandList(): Array<vscode.Disposable> {
+  return [
+    vscode.commands.registerCommand('clean-css-vscode.Format', () => format()),
+    vscode.commands.registerCommand('clean-css-vscode.FastBeauty', () => format({format: 'beautify'})),
+    vscode.commands.registerCommand('clean-css-vscode.FastKBreaks', () => format({format: 'keep-breaks'})),
+    vscode.commands.registerCommand('clean-css-vscode.FastCompact', () => format({}))
+  ]
 }
 
-function debugInfo (output: any){
-  debugCh.clear();
-  debugCh.appendLine('ERRORS:'); let i = 0;
-  if(output.errors.length){output.errors.forEach((element: any) => {
-    debugCh.appendLine("\t"+(++i)+". "+element);
-  })}else {debugCh.appendLine('\tNo errors found')}
-  debugCh.appendLine('WARNINGS:'); i = 0;
-  if(output.warnings.length){output.warnings.forEach((element: any) => {
-    debugCh.appendLine("\t"+(++i)+". "+element);
-  })}else {debugCh.appendLine('\tNo warnings founds')}
-  debugCh.appendLine('STATS:')
-  debugCh.appendLine("\t1. File size: "+output.stats.originalSize+"B (before) -> "+output.stats.minifiedSize+"B (now)")
-  debugCh.appendLine("\t2. Efficiency: "+(output.stats.efficiency*100).toFixed(2) + "%")
-  debugCh.appendLine("\t3. Time Spent: "+output.stats.timeSpent+"ms")
+//for "clean-css-vscode.Format" command and others
+function format(option?: object){
+  options = vscode.workspace.getConfiguration('cleanCSS');
+  let editor = vscode.window.activeTextEditor;
+  if (editor !== undefined) {
+    new cleanCSS(option ?? options.formatterOptions)
+      .minify(editor.document.getText(), (error: object, output: { styles: string }) => CleanCSSCallback(error, output, editor));
+  } else {
+    vscode.window.showErrorMessage("Editor not available");
+  }
+}
+
+//Use the Debug Output Channel
+function debugInfo(output: any) {
+  if (options.debugTool.showMessage) {
+    debugCh.clear();
+    debugCh.show();
+    // Show a list of errors.
+    debugCh.appendLine('ERRORS:');
+    if (output.errors.length) {
+      output.errors.forEach((element: any, index: number) => {
+        debugCh.appendLine("\t" + index + ". " + element);
+      })
+    } else { debugCh.appendLine('\tNo errors found') }
+    // Show a list of warnings.
+    debugCh.appendLine('WARNINGS:');
+    if (output.warnings.length) {
+      output.warnings.forEach((element: any, index: number) => {
+        debugCh.appendLine("\t" + index + ". " + element);
+      })
+    } else { debugCh.appendLine('\tNo warnings founds') }
+    debugCh.appendLine('STATS:');
+    // Clean CSS Doc: Using inline options is going to change the size of the file. This shows the stats of the file AFTER the change.
+    debugCh.appendLine("\t1. File size: " + output.stats.originalSize + "B (before) -> " + output.stats.minifiedSize + "B (now)");
+    debugCh.appendLine("\t2. Efficiency: " + (output.stats.efficiency * 100).toFixed(2) + "%");
+    debugCh.appendLine("\t3. Time Spent: " + output.stats.timeSpent + "ms");
+  }
+}
+
+//Callback for "minify" function of Clean CSS
+function CleanCSSCallback(error: object,output: {styles: string}, editor?: vscode.TextEditor){
+  // This 'cause typescript need to verify "editor" existence every time
+  if (editor) {
+      editor.edit(edit => { edit.replace(
+        new vscode.Range(0, 0, 
+          editor.document.lineCount - 1, 
+          editor.document.lineAt(editor.document.lineCount - 1).text.length),
+        output.styles)
+    });
+    //Use Output channel to info.
+    debugInfo(output); 
+  } else {
+    vscode.window.showErrorMessage(error.toString());
+  }
 }
